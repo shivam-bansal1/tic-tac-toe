09ffba2e5e274e7e96496f67664d85a4fa97e17f
Shivam Bansal <shivambnsal@gmail.com>
2024-08-28 21:12:55 +0530

    Create console version of game
    - Code the game from the inside out
    - Make the game playable in console
    - Display gameBorad in console in this commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Tic Tac Toe");
+function GameBoard() {
+    const board = [];
+    const rows = 3;
+    const columns = 3;
+    let cellNumber = 0;
+
+    for(let r=0; r<rows; r++) {
+        board[r] = [];
+
+        for(let c=0; c<columns; c++) {
+            board[r][c] = cellNumber++;
+        }
+    }
+
+    const getBoard = () => board ;
+
+    return { getBoard };
+}
+
+const board = GameBoard();
+console.log(board.getBoard());
