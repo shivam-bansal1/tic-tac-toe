6e2319a92f6bbd3721a1aff7bf01e5893e23920e
Shivam Bansal <shivambnsal@gmail.com>
2024-09-06 01:26:14 +0530

    Create board and switch player functionality

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
 /*
 Optimized Version - 1. Using Array() to make gameboard instead of 2-D matrix
 */
+
+function Gameboard() {
+    // Create game board
+    const board = Array(9).fill("");
+
+    // Return game board
+    const getBoard = () => board;
+
+    return { getBoard };
+}
+
+function GameController() {
+    const board = Gameboard();
+
+    console.log(board.getBoard());
+
+    const players = [
+        {
+            name: "Player One",
+            token: "x"
+        },
+        {
+            name: "Player Two",
+            token: "o"
+        }
+    ]
+
+    const activePlayer = players[0];
+
+    const getActivePlayer = () => activePlayer ;
+    const switchActivePlayer = () => activePlayer === players[0] ? players[1] : players[0];
+
+    return {
+        getActivePlayer,
+        getBoard: board.getBoard,
+        switchActivePlayer,
+    }
+}
+
+const game = GameController();
+let activePlayer = game.getActivePlayer()
+console.log(`activePlayer: ${activePlayer.name}`);
+activePlayer = game.switchActivePlayer();
+console.log(`activePlayer: ${activePlayer.name}`);
