3e532eac52d6bf1c930c83488184376ad5c5e827
Shivam Bansal <shivambnsal@gmail.com>
2024-09-07 22:17:27 +0530

    Functionality to add token

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ function Gameboard() {
 
     // Return game board
     const getBoard = () => board;
+    
+    const placeToken = (token, cellNumber) => {
+        if(board[cellNumber] === "") {
+            board[cellNumber] = token;
+            return true;
+        }
+        return true;
+    }
 
-    return { getBoard };
+    return { getBoard, 
+            placeToken };
 }
 
 function GameController() {
@@ -28,20 +37,30 @@ function GameController() {
         }
     ]
 
-    const activePlayer = players[0];
+    let activePlayer = players[0];
 
     const getActivePlayer = () => activePlayer ;
-    const switchActivePlayer = () => activePlayer === players[0] ? players[1] : players[0];
+    const switchActivePlayer = () => activePlayer = activePlayer === players[0] ? players[1] : players[0];
 
     return {
         getActivePlayer,
         getBoard: board.getBoard,
         switchActivePlayer,
+        placeToken: board.placeToken,
     }
 }
 
 const game = GameController();
+
 let activePlayer = game.getActivePlayer()
 console.log(`activePlayer: ${activePlayer.name}`);
+
+game.placeToken(activePlayer.token, 2);
+console.log(game.getBoard());
+
 activePlayer = game.switchActivePlayer();
-console.log(`activePlayer: ${activePlayer.name}`);
+console.log(`activePlayer: ${game.getActivePlayer().name}`);
+
+game.placeToken(activePlayer.token, 0);
+console.log(game.getBoard());
+
